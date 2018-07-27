fbe671244bde76fec3ea63bd4e6bd06295a96993
Kyle Miller <kylmil03@protiviti.com>
2018-07-27 09:14:25 -0500

    Raised Max Value to 7

--- riddles/forms.py
+++ riddles/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 
 class TeamSelectionForm(forms.Form):
-    teamNum = forms.IntegerField(label="Team Number", max_value=6, min_value=1)
+    teamNum = forms.IntegerField(label="Team Number", max_value=7, min_value=1)
 
 class RiddleSubmissionForm(forms.Form):
 	submission = forms.CharField(label="Answer")
